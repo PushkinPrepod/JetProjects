28a57632b955540e0b23316f0a5d9062f7703bdf
pyshkin-als <95368682+pyshkin-als@users.noreply.github.com>
2021-12-01 19:10:19 +0300

    Add files via upload

--- ychilka.cpp
+++ ychilka.cpp
@@ -5,10 +5,20 @@ using namespace std;
 int main() {
 	ofstream f;
 	f.open("failiychilki.opentxt");
-	cout<<"vvedi"<<endl;
+	 cout<<"vvedi"<<endl;
 	string nf;
 	cin>>nf;
 	f<<nf;
 	cin>>nf;
 	f<<nf;
+	
+	for (int i=1; i<100; i++) {
+		f<<i;
+	}
+	
+	cout<<"Yes"<<endl;
+	
+	
+	
 }
+
